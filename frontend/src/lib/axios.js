518237a350a9e513f31e96accdbc009fc9e00de9
Shahrukh777 <sheikhshahrukh904@gmail.com>
2025-11-24 17:00:45 +0530

    last test

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL =
   import.meta.env.DEV
     ? "http://localhost:3000/api"
-    : `${window.location.origin}/api`;
+    : `https://thinkpad-38lg.onrender.com/api`;
 
 const api = axios.create({
   baseURL: BASE_URL,
