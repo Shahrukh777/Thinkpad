70826692c36502beda301962da542b29f750f392
Shahrukh777 <sheikhshahrukh904@gmail.com>
2025-11-24 16:54:35 +0530

    testing for deployment 4

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,12 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000/api" :"/api"
+const BASE_URL =
+  import.meta.env.DEV
+    ? "http://localhost:3000/api"
+    : `${window.location.origin}/api`;
 
 const api = axios.create({
-    baseURL: BASE_URL,
-})
+  baseURL: BASE_URL,
+});
 
-export default api
+export default api;
